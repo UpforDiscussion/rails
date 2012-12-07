8e13219981dcdb9c044cacb82b6235b6170477d3
Avinash Karnani <avi@avikarnani.com>
2012-12-07 14:25:39 -0500

    first commit

---
+++ labs/google-analytics/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleAnalytics::Application.config.secret_token = '242f030233eaa5c092b247c27a0a05ea42b35ab25d8f8f51e77887e83dde724f6916b3ffee0c7df43f2ead47d03f46814bcbb61f3356edb50365b60d6f29e71a'
